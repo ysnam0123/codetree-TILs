684d7438baded1a3d588d768cf7bffdfc29dfc7d
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-02-22 01:51:52 +0900

    Title: 두수의 곱과 몫, Time: 190ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240222/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,9 @@
+// 입력 및 변수 선언
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+console.log("300 * 5 = 1500");
+console.log("300 / 5 = 60");
