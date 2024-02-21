aa60e280c7c76bea0f933662d0f86c913038f3cf
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-02-22 00:50:10 +0900

    Title: 전화번호 바꾸기, Time: 207ms, Memory: 47MB, Status: Passed - CodeTree

--- 240222/전화번호 바꾸기/change-phone-number.js
+++ 240222/전화번호 바꾸기/change-phone-number.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split(".");
+let input = fs.readFileSync(0).toString().trim().split("-");
 let a = input[0];
 let b = input[1];
 let c = input[2];
