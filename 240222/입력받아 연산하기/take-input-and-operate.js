2c1c28adde16c256c80c3833019c001be92c6d77
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-02-22 02:13:43 +0900

    Title: 입력받아 연산하기, Time: 182ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240222/입력받아 연산하기/take-input-and-operate.js
@@ -0,0 +1,11 @@
+// 입력 및 변수 선언
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+a +=87;
+b %= 10;
+console.log(a);
+console.log(b);
