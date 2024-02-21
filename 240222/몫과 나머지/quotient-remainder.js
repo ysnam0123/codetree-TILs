e74ea6987be7f97e08239cf0da3577f76904e076
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-02-22 02:10:42 +0900

    Title: 몫과 나머지, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240222/몫과 나머지/quotient-remainder.js
@@ -0,0 +1,10 @@
+// 입력 및 변수 선언
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let c = parseInt(a/b);
+let d = a%b;
+console.log(c+`...`+d);
