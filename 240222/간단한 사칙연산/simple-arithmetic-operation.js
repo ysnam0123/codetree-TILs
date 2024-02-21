5e7a9ba705944863a97dd3ea1e7c469383cc398e
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-02-22 01:10:28 +0900

    Title: 간단한 사칙연산, Time: 234ms, Memory: 14MB, Status: Passed - CodeTree

--- 240222/간단한 사칙연산/simple-arithmetic-operation.js
+++ 240222/간단한 사칙연산/simple-arithmetic-operation.js
@@ -1,6 +1,12 @@
-let a = 2
-let b = 5
-console.log(a+b);
-console.log(a-b);
-console.log(parseInt(a/b));
-console.log(a%b);
+// 입력 및 변수 선언
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+// 출력
+console.log(a + b)
+console.log(a - b)
+console.log(parseInt(a / b))
+console.log(a % b)
