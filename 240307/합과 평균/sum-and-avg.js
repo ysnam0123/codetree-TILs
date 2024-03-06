aac94f316e8234c1b0cabc6e024ecb88204a433a
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-03-07 00:06:26 +0900

    Title: 합과 평균, Time: 178ms, Memory: 14MB, Status: Passed - CodeTree

--- 240307/합과 평균/sum-and-avg.js
+++ 240307/합과 평균/sum-and-avg.js
@@ -1,3 +1,12 @@
-let a=2;
-let b=5;
-console.log(a+b,(a+b)/2);
+// 입력 및 변수 선언
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+// 평균 구하기
+let average = (a + b) / 2;
+
+// 출력
+console.log(a + b, average.toFixed(1));
