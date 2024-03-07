121a9833274cf340914e1fb22a15eeb1cba2f210
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-03-07 09:58:57 +0900

    Title: 합과 평균의 차, Time: 178ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240307/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,16 @@
+// 입력 및 변수 선언
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+// 평균 구하기
+// 소수점을 버려야하기 때문에 parseInt()를 사용합니다.
+let average = ((a + b + c) / 3);
+
+// 출력
+console.log(a + b + c)
+console.log(average);
+console.log((a+b+c)-(average));
