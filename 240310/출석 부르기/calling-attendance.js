e1515148a7d8fdccf1c5b19bbba1f606a5192641
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-03-10 16:32:16 +0900

    Title: 출석 부르기, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240310/출석 부르기/calling-attendance.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+let input = Number(fs.readFileSync(0).toString().trim());
+
+if (input=1) {
+    console.log("John");
+}
+else if(input=2){
+    console.log("Tom");
+}
+else if(input=3){
+    console.log("Paul");
+}
+else {
+    console.log("Vacancy")
+}
