b7a620e714e49c1ca476a2c61d1ced888f934205
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-03-10 16:42:20 +0900

    Title: 등급 매기기, Time: 304ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240310/등급 매기기/ranking.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+let input = Number(fs.readFileSync(0).toString().trim());
+
+if (input>=90) {
+    console.log("A");
+}
+else if (input>=80) {
+    console.log("B");
+}
+else if (input >=70){
+    console.log("C");
+}
+else if (input >=60) {
+    console.log("D");
+}
+else {
+    console.log("F");
+}
