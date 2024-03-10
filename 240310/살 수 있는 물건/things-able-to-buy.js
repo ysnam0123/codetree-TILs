831c23571abaf983b1dd71b2c34ff396c16448e0
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-03-10 16:28:58 +0900

    Title: 살 수 있는 물건, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240310/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,12 @@
+const fs=require("fs");
+let input = fs.readFileSync(0).toString().trim();
+let a = Number(input);
+if (a>=3000) {
+    console.log("book");
+}
+else if (a>=1000){
+    console.log("mask");
+}
+else {
+    console.log("no");
+}
