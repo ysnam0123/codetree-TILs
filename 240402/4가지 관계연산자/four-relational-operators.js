fd8198426932ef4d3ad9aa77cd9fec4c388c62a2
ysnam0123 <158164219+ysnam0123@users.noreply.github.com>
2024-04-02 00:16:09 +0900

    Title: 4가지 관계연산자, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240402/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,36 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+if (a>=b) {
+    console.log("1")
+}
+
+else {
+    console.log("0");
+}
+
+if (a>b) {
+    console.log("1")
+}
+
+else {
+    console.log("0");
+}
+
+if (b>=a) {
+    console.log("1")
+}
+
+else {
+    console.log("0");
+}
+if (b>a) {
+    console.log("1")
+}
+
+else {
+    console.log("0");
+}
